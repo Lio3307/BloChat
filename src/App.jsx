88671a9b829bd008a6110f7d80bd587d882618af
Lio3307 <aureliofs01@gmail.com>
2025-06-13 15:24:09 +0800

    make web remember user session

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./Pages/Login";
 import { Register } from "./Pages/Register";
 import { Profile } from "./components/Profile";
+import { auth } from "./firebase/config";
 
 function App() {
+
+  const  [rememberUser, setRememberUser] = useState("")
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setRememberUser(user)
+    })
+  })
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={rememberUser ? <Navigate to={"/profile"}/> : <Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile/>}/>
       </Routes>
